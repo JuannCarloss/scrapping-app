b4cc2a61e3587ec5551664d189135e02654ddd2d
juanc <juancarlosbatistazeferino@gmail.com>
2025-10-16 10:31:30 -0300

    feat: project first configs

---
+++ src/test/java/com/scrap/knewin/KnewinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrap.knewin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnewinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
