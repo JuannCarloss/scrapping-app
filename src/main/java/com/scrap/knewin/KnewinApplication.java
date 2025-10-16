8bf8564c2553cd89f2527cf927c218a50517a9f3
juanc <juancarlosbatistazeferino@gmail.com>
2025-10-16 10:34:39 -0300

    feat: add main class

---
+++ src/main/java/com/scrap/knewin/KnewinApplication.java
@@ -0,0 +1,13 @@
+package com.scrap.knewin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnewinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnewinApplication.class, args);
+	}
+
+}
